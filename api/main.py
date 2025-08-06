8f2959aaece5cd843e8becd83f0a5b9a4cec689c
BMDarkLight <72068517+BMDarkLight@users.noreply.github.com>
2025-08-06 20:09:25 +0330

    fix: Minor Swagger Fix

--- api/main.py
+++ api/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.openapi.utils import get_openapi
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 from typing import List, Optional, Dict
+
+import os
 
 app = FastAPI(title="Organizational AI API")
 
